8b4db1e102adeb910741dc857ca7aa8d3bf25bee
Narayan Bhadada <narayan.bhadada@CIGNEXDATAMATICS.COM>
2019-01-25 17:02:12 +0530

    Narayan

---
+++ src/main/java/com/cignex/controller/AppSecurity.java
@@ -0,0 +1,54 @@
+package com.cignex.controller;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import net.bytebuddy.agent.builder.AgentBuilder.InitializationStrategy.NoOp;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurity extends WebSecurityConfigurerAdapter
+{
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Bean
+	public AuthenticationProvider authProvider()
+	{
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		return provider;
+	}
+	
+	
+	
+	
+	/*@SuppressWarnings("deprecation")
+	@Bean
+	@Override
+	protected UserDetailsService userDetailsService() 
+	{
+		List<UserDetails> user = new ArrayList<>();
+		user.add(User.withDefaultPasswordEncoder().username("narayan").password("1234").roles("USER").build());
+		return new InMemoryUserDetailsManager(user);
+		*/
+				
+		
+	}
+	
+
+
+
